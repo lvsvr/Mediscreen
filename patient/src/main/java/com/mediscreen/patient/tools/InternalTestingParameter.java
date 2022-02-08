37276e14e669c27672efbe4bc4d5738d38370470
w-boar <av.w.boar@gmail.com>
2022-02-08 07:59:47 +0100

    Docker

--- patient/src/main/java/com/mediscreen/patient/tools/InternalTestingParameter.java
+++ patient/src/main/java/com/mediscreen/patient/tools/InternalTestingParameter.java
@@ -1,31 +1,31 @@
-//=========================================
-//  uncomment  :
+////=========================================
+////  uncomment  :
+////
+////     internalPatient
+////  + internalTestingController
+////  + internalTestingParameter
+////  =
+//// GET a list of internal patients:
+//// http://localhost:9001/testing/initializeInternalPatients
+////
+//// GET all the patients from the db:
+//// http://localhost:9001/testing/resetDb
+////=========================================
 //
-//     internalPatient
-//  + internalTestingController
-//  + internalTestingParameter
-//  =
-// GET a list of internal patients:
-// http://localhost:9001/testing/initializeInternalPatients
+//package com.mediscreen.patient.tools;
 //
-// GET all the patients from the db:
-// http://localhost:9001/testing/resetDb
-//=========================================
-
-package com.mediscreen.patient.tools;
-
-/**
- * The type InternalTestingParameter - parameter for InternalPatient (testing tool)
- */
-public class InternalTestingParameter {
-
-    private static int internalPatientNumber = 10;
-
-    public static void setInternalPatientNumber(int internalPatientNumber) {
-        InternalTestingParameter.internalPatientNumber = internalPatientNumber;
-    }
-
-    public static int getInternalPatientNumber() {
-        return internalPatientNumber;
-    }
-}
+///**
+// * The type InternalTestingParameter - parameter for InternalPatient (testing tool)
+// */
+//public class InternalTestingParameter {
+//
+//    private static int internalPatientNumber = 10;
+//
+//    public static void setInternalPatientNumber(int internalPatientNumber) {
+//        InternalTestingParameter.internalPatientNumber = internalPatientNumber;
+//    }
+//
+//    public static int getInternalPatientNumber() {
+//        return internalPatientNumber;
+//    }
+//}
